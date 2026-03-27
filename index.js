973e09e23cf1b124fd40f718ba978b387d6a27e1
Mateus-Cimini <mateusmateus122333@gmail.com>
2026-03-27 13:00:30 -0300

    feat: att

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const { marked } = require("marked");
+
+const md = fs.readFileSync("README.md", "utf-8");
+
+const html = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Meu Markdown</title>
+</head>
+<body>
+  ${marked(md)}
+</body>
+</html>
+`;
+
+fs.writeFileSync("index.html", html);
+
+console.log("HTML gerado!");
